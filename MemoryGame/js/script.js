8708290d17422dcff3fd612002b8e7165ff7303a
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2022-02-17 21:02:55 +0100

    Implement card matching

--- MemoryGame/js/script.js
+++ MemoryGame/js/script.js
@@ -19,25 +19,87 @@ function makeCards(size){
         }
         wrapper.appendChild(container);
     }
+}
 
-
-};
+let cards, openedCards, cardsAsArray;
 
 document.getElementById("lvl1").addEventListener("click", () => {
     document.getElementById("wrapper").innerHTML = "";
     makeCards(2);
+    openedCards = [];
+    cards = document.getElementsByClassName("card");
+    cardsAsArray = Array.from(cards);
 });
 
 document.getElementById("lvl2").addEventListener("click", () => {
     document.getElementById("wrapper").innerHTML = "";
     makeCards(4);
+    openedCards = [];
+    cards = document.getElementsByClassName("card");
+    cardsAsArray = Array.from(cards);
 });
 
 document.getElementById("lvl3").addEventListener("click", () => {
     document.getElementById("wrapper").innerHTML = "";
     makeCards(6);
+    openedCards = [];
+    cards = document.getElementsByClassName("card");
+    cardsAsArray = Array.from(cards);
 });
 
 function turnCard() {
-    window.alert("hey")
-};
+    if (openedCards.length == 1 && openedCards[0] == this) {
+        openedCards = [];
+        this.style.background = "blue";
+        return;
+    }
+
+    if (openedCards.length < 2) {
+        openedCards.push(this);
+        this.style.background = "#7777FF";
+        
+        if (openedCards.length == 2) {
+            if (openedCards[0].innerHTML === openedCards[1].innerHTML) {
+                setTimeout(() => {
+                    cardsAsArray.forEach(element => {
+                        if (element.innerHTML === this.innerHTML) {
+                            element.style.background = "green";
+                            element.removeEventListener("click", turnCard);
+                        }
+                    });
+                }, 500);
+
+                setTimeout(() => {
+                    for (let i = 0; i < cardsAsArray.length; i++) {
+                        if (cardsAsArray[i].innerHTML == this.innerHTML) {
+                            cardsAsArray.splice(i, 1);
+                            i--;
+                        }
+                    }
+                }, 500);
+
+                setTimeout(() => {
+                    if (cardsAsArray.length == 0) {
+                        window.alert("You win!");
+                    }
+                }, 1000);
+                
+                openedCards = [];
+            }
+            else {
+                setTimeout(() => {
+                    for (let i = 0; i < cardsAsArray.length; i++) {
+                        if (cardsAsArray[i] == openedCards[0]) {
+                            cardsAsArray[i].style.background = "blue";
+                        }
+                    }
+                    this.style.background = "blue";
+                }, 500);
+
+                setTimeout(() => {
+                    openedCards = []
+                }, 501);   
+            }   
+        }
+    }
+}
