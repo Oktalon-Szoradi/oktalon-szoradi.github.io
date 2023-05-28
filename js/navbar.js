c769a6ea905f6b90ea64dbc46445f77be74ed732
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2023-05-28 05:15:57 +0200

    Revamp! All in one lol. This isn't really what Git is for...

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+const navbar = document.querySelector('.navbar')
+const navbarMenuHolder = document.querySelector('.navbar-menu-holder')
+// const navbarMenu = document.querySelector('.navbar-menu')
+const navbarMenuButton = document.querySelector('.navbar-menu-button')
+const navbarMenuUnderlay = document.querySelector('.navbar-menu-underlay')
+
+const toggleNavbarMenu = () => {
+  navbar.classList.toggle('navbar-menu-isopen')
+  navbarMenuButton.classList.toggle('active')
+  navbarMenuHolder.classList.toggle('hidden')
+  // navbarMenu.classList.toggle('hidden')
+  // navbarMenuUnderlay.classList.toggle('hidden')
+}
+
+navbarMenuButton.addEventListener('click', toggleNavbarMenu)
+navbarMenuUnderlay.addEventListener('click', toggleNavbarMenu)
