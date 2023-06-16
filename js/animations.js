659a7f3f33d8c952492014e4b75808b52f297a1a
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2023-06-16 23:08:14 +0200

    No JS Compatibility

--- js/animations.js
+++
@@ -1,8 +0,0 @@
-const boxes = document.querySelectorAll('.box')
-
-for (let i = 0; i < boxes.length; i++) {
-  setTimeout(() => {
-    boxes[i].classList.add('display-block')
-    boxes[i].classList.add('appear-animation')
-  }, i * 100) // Change the delay time (in milliseconds) as needed
-}
