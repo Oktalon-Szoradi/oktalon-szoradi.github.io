d722968d767d634a2894b9c0658f0c396d8eea93
Oktalon-Szoradi <szoradi.school2@gmail.com>
2025-08-07 01:45:56 +0200

    npm run format

--- src/components/CopyToClipboard.vue
+++ src/components/CopyToClipboard.vue
@@ -63,8 +63,11 @@ $height: 5px;
   /* margin-right: 1px; */
   border: none;
   border-radius: #{$corner-roundness + 1px} 0 0 #{$corner-roundness + 1px};
-  box-shadow: inset 0 0 0 1px hsl(0deg 0% 0% / 75%), inset 0 0 0 2px hsl(0deg 0% 100% / 25%),
-    inset 1px 1px 8px hsl(0deg 0% 0% / 50%), 0 1px hsl(0deg 0% 100% / 25%);
+  box-shadow:
+    inset 0 0 0 1px hsl(0deg 0% 0% / 75%),
+    inset 0 0 0 2px hsl(0deg 0% 100% / 25%),
+    inset 1px 1px 8px hsl(0deg 0% 0% / 50%),
+    0 1px hsl(0deg 0% 100% / 25%);
   background: linear-gradient(to bottom, transparent, hsl(0deg 0% 15% / 50%));
   padding: 0.25em 0.75em;
   height: 27px;
@@ -75,9 +78,17 @@ $height: 5px;
 
   &:focus-visible {
     outline: none;
-    box-shadow: 0 0 8px 4px hsl(211deg 64% 68%), inset 0 0 0 1px hsl(0deg 0% 0% / 75%),
-      inset 0 0 0 2px hsl(0deg 0% 100% / 50%), inset 1px 1px 8px hsl(0deg 0% 0% / 50%), 0 1px hsl(0deg 0% 100% / 25%);
-    background: linear-gradient(to bottom, hsl(0deg 0% 100% / 10%), hsl(0deg 0% 25% / 50%));
+    box-shadow:
+      0 0 8px 4px hsl(211deg 64% 68%),
+      inset 0 0 0 1px hsl(0deg 0% 0% / 75%),
+      inset 0 0 0 2px hsl(0deg 0% 100% / 50%),
+      inset 1px 1px 8px hsl(0deg 0% 0% / 50%),
+      0 1px hsl(0deg 0% 100% / 25%);
+    background: linear-gradient(
+      to bottom,
+      hsl(0deg 0% 100% / 10%),
+      hsl(0deg 0% 25% / 50%)
+    );
   }
 }
 
@@ -132,12 +143,32 @@ $height: 5px;
   position: relative;
   transition-duration: vars.$transdur-mouseleave;
   border-radius: 0 #{$corner-roundness} #{$corner-roundness} 0;
-  box-shadow: inset 0 0 0 1px hsl(0deg 0% 100% / 25%), 0 0 0 1px hsl(0deg 0% 0% / 75%);
-  background: vars.$reflection-soft, vars.$reflection-weak,
-    radial-gradient(ellipse 100% 33% at bottom, hsl(0deg 0% 100% / 10%), transparent),
-    radial-gradient(ellipse at bottom left, transparent 75%, hsl(0deg 0% 100% / 15%)),
-    radial-gradient(ellipse at bottom right, transparent 75%, hsl(0deg 0% 100% / 15%)),
-    linear-gradient(to bottom, hsl(0deg 0% 0% / 10%), hsl(0deg 0% 0% / 30%), hsl(0deg 0% 0% / 20%));
+  box-shadow:
+    inset 0 0 0 1px hsl(0deg 0% 100% / 25%),
+    0 0 0 1px hsl(0deg 0% 0% / 75%);
+  background:
+    vars.$reflection-soft, vars.$reflection-weak,
+    radial-gradient(
+      ellipse 100% 33% at bottom,
+      hsl(0deg 0% 100% / 10%),
+      transparent
+    ),
+    radial-gradient(
+      ellipse at bottom left,
+      transparent 75%,
+      hsl(0deg 0% 100% / 15%)
+    ),
+    radial-gradient(
+      ellipse at bottom right,
+      transparent 75%,
+      hsl(0deg 0% 100% / 15%)
+    ),
+    linear-gradient(
+      to bottom,
+      hsl(0deg 0% 0% / 10%),
+      hsl(0deg 0% 0% / 30%),
+      hsl(0deg 0% 0% / 20%)
+    );
   width: 100%;
   height: 100%;
   text-shadow: 0 2px 2px hsl(0deg 0% 0% / 25%);
@@ -193,7 +224,9 @@ button {
     .face {
       transform: translateY(#{$height});
       transition-duration: vars.$transdur-press;
-      box-shadow: inset 0 0 0 1px hsl(0deg 0% 100% / 25%), 0 0 0 1px hsl(0deg 0% 0% / 75%),
+      box-shadow:
+        inset 0 0 0 1px hsl(0deg 0% 100% / 25%),
+        0 0 0 1px hsl(0deg 0% 0% / 75%),
         inset 0 0 12px hsl(0deg 0% 0% / 75%);
       color: hsl(0deg 0% 100% / 50%);
       filter: brightness(0.75);
@@ -220,7 +253,9 @@ button {
   .face {
     transform: translateY(#{$height});
     transition-duration: vars.$transdur-press;
-    box-shadow: inset 0 0 0 1px hsl(0deg 0% 100% / 25%), 0 0 0 1px hsl(0deg 0% 0% / 75%),
+    box-shadow:
+      inset 0 0 0 1px hsl(0deg 0% 100% / 25%),
+      0 0 0 1px hsl(0deg 0% 0% / 75%),
       inset 0 0 12px hsl(0deg 0% 0% / 75%);
     color: hsl(0deg 0% 100% / 50%);
   }
